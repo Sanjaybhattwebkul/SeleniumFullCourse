e0cdb66410a47f91cc35dda530121d13612c228f
SanjuDeveloper <bhattsanju41@gmail.com>
2022-06-26 01:13:34 -0700

    CssSelector-Implemented

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/29/5033eb8225f5001c19daeb8f146bd033
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\chrome-exe\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("Rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("Rahul@123");
+		driver.findElement(By.className("signInBtn")).click();
+
+	}
+
+}
--- SelinumBesic/src/Locators.java
+++ SelinumBesic/src/Locators.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,10 +10,12 @@ public class Locators {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\chrome-exe\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("Rahul");
 		driver.findElement(By.name("inputPassword")).sendKeys("Rahul@123");
 		driver.findElement(By.className("signInBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 
 	}
 
