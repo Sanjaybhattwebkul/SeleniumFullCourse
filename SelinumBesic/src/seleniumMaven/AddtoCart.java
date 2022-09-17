584c7a3c42ce4dea510955eaf46c831b1a0a6017
SanjuDeveloper <bhattsanju41@gmail.com>
2022-09-17 05:28:35 -0700

    Code-formatted

--- SelinumBesic/src/seleniumMaven/AddtoCart.java
+++ SelinumBesic/src/seleniumMaven/AddtoCart.java
@@ -16,44 +16,34 @@ public class AddtoCart {
 		System.setProperty("webdriver.chrome.driver", "C:\\chrome-exe\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.manage().window().maximize(); 
-		
+		driver.manage().window().maximize();
+
 		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot" };
-		
+
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 		Thread.sleep(3000);
 		addItems(driver, itemsNeeded);
 	}
 
-	public static  void addItems(WebDriver driver,String[] itemsNeeded)
-	{
+	public static void addItems(WebDriver driver, String[] itemsNeeded) {
+		int j = 0;
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+		for (int i = 0; i < products.size(); i++) {
+			// Brocolli - 1 kg
+			// Brocolli, 1 kg
+			String[] name = products.get(i).getText().split("-");
+			String formattedName = name[0].trim(); // format it to get actual vegetable name
+			List itemsNeededList = Arrays.asList(itemsNeeded); // convert array into array list for easy search
 
-			int j=0;
-			List<WebElement> products=driver.findElements(By.cssSelector("h4.product-name"));
-			for(int i=0;i<products.size();i++) {	
-				//Brocolli - 1 Kg
-				//Brocolli,    1 kg
-				String[] name=products.get(i).getText().split("-");
-				String formattedName=name[0].trim();
-				
-				//format it to get actual vegetable name
-				//convert array into array list for easy search
-				//  check whether name you extracted is present in arrayList or not-
-		
-					List itemsNeededList = Arrays.asList(itemsNeeded);
-					
-					if(itemsNeededList.contains(formattedName))	{
-					j++;
-			
-					//click on Add to cart
-					driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
-					
-					if(j==itemsNeeded.length) {
-						break;
-					}
-		
+			if (itemsNeededList.contains(formattedName)) {
+				j++;
+				// click on Add to cart
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+				if (j == itemsNeeded.length) {
+					break;
 				}
 
 			}
 		}
+	}
 }
