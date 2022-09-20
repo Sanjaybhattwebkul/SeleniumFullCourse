f48848dbd0572b2681cfbe258556b5deb5bfd0c8
SanjuDeveloper <bhattsanju41@gmail.com>
2022-09-20 09:49:49 -0700

    Date-select-from-calendar-Automated

--- SelinumBesic/src/seleniumMaven/CalendarHandle.java
+++ SelinumBesic/src/seleniumMaven/CalendarHandle.java
@@ -1,4 +1,5 @@
 package seleniumMaven;
+
 import java.time.Duration;
 import java.util.List;
 import org.openqa.selenium.By;
@@ -15,29 +16,26 @@ public class CalendarHandle {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 		driver.get("https://www.path2usa.com/travel-companions");
-		JavascriptExecutor js=(JavascriptExecutor)driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,950)");
-
 		// April 23
-		driver.findElement(By.xpath("//input[@id='form-field-travel_comp_date']")).click();
-		
-		while (!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText()
-				.contains("May")) {
-			driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id='form-field-travel_comp_date']")).click();
+		while (!driver.findElement(By.xpath("//div[@class='flatpickr-month']")).getText().contains("October")) {
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//span[@class='flatpickr-next-month']")).click();
 		}
 
-		List<WebElement> dates = driver.findElements(By.className("day"));
-		// Grab common attribute//Put into list and iterate
-		int count = driver.findElements(By.className("day")).size();
-
-		for (int i = 0; i < count; i++) {
-			String text = driver.findElements(By.className("day")).get(i).getText();
-			if (text.equalsIgnoreCase("21")) {
-				driver.findElements(By.className("day")).get(i).click();
+		int count = driver.findElements(By.xpath("//span[@class='flatpickr-day ']")).size();
+		for (int i = 0; i < count; i++)	{
+			String text = driver.findElements(By.xpath("//span[@class='flatpickr-day ']")).get(i).getText();
+			if (text.equalsIgnoreCase("26")) {
+				driver.findElements(By.xpath("//span[@class='flatpickr-day ']")).get(i).click();
+				System.out.println(text);
 				break;
 			}
-
 		}
+		System.out.println(driver.findElement(By.xpath("//*[@id='form-field-travel_comp_date']")).getAttribute("value"));
 
 	}
 
