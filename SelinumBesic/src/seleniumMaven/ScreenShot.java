81df1597b9f9227b6bbe6885da724acd5dfabbaf
SanjuDeveloper <bhattsanju41@gmail.com>
2022-09-21 12:43:46 -0700

    Screenshot-of Specific-element

--- SelinumBesic/src/seleniumMaven/ScreenShot.java
+++ SelinumBesic/src/seleniumMaven/ScreenShot.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ScreenShot {
@@ -18,8 +20,15 @@ public class ScreenShot {
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		
+		// Screenshot of Full webPagr
 		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(src,new File("D:\\SeleniumScreenshots\\logo.png"));
+		
+		// Screenshot of specific element
+		WebElement element=driver.findElement(By.cssSelector("a.blinkingText"));
+		File src1=element.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src1,new File("D:\\SeleniumScreenshots\\element.png"));
+
 		
 
 	}
