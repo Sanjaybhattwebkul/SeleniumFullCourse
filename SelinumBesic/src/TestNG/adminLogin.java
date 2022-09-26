e9e2f2b6e089d533d2b7663c9aaae81668f269b8
SanjuDeveloper <bhattsanju41@gmail.com>
2022-09-26 11:59:20 -0700

    TestNG started

---
+++ SelinumBesic/src/TestNG/adminLogin.java
@@ -0,0 +1,15 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class adminLogin {
+	@Test
+	public static void demo1() {
+		System.out.println("Admin login  ONE");
+	}
+	
+	@Test
+	public static void demo2() {
+		System.out.println("admin Login TWO");
+	}
+}
---
+++ SelinumBesic/src/TestNG/test.java
@@ -0,0 +1,17 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class test {
+
+	@Test
+	public static void test1() {
+		System.out.println("Hello testNG ONE");
+	}
+	
+	@Test
+	public static void test2() {
+		System.out.println("Hello testNG TWO");
+	}
+
+}
