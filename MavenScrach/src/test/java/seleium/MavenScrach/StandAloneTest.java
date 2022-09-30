310cd38d68f306fd1cecf3c81419b6e6063e377e
SanjuDeveloper <bhattsanju41@gmail.com>
2022-09-30 12:12:18 -0700

    AddTOCart using JavaStream and Maven

--- MavenScrach/src/test/java/seleium/MavenScrach/StandAloneTest.java
+++ MavenScrach/src/test/java/seleium/MavenScrach/StandAloneTest.java
@@ -1,6 +1,12 @@
 package seleium.MavenScrach;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,7 +17,24 @@ public class StandAloneTest {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://rahulshettyacademy.com/client/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client");
+		driver.findElement(By.id("userEmail")).sendKeys("tom@example.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Tom@1234");
+		driver.findElement(By.id("login")).click();
+		
+		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+		
+		//Optional<String> actualProduct = products.stream().map(product->product.findElement(By.cssSelector("b")).getText()).findFirst();
+		
+		//System.out.println("Product Nmae -: "+ actualProduct.isEmpty());
+		
+		WebElement actualProduct = products.stream().filter(product->
+		product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+		
+		actualProduct.findElement(By.cssSelector(".card-body button:last-of-type")).click(); // agr 2 button h to 2 vala find hoga.
+		
 	}
 
 }
