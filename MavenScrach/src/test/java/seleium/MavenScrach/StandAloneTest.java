f7f00498d8ecd6a40f7becb7eec7be7c8a2e2822
SanjuDeveloper <bhattsanju41@gmail.com>
2022-09-30 13:57:00 -0700

    explicit Waites implemented for syncronization

--- MavenScrach/src/test/java/seleium/MavenScrach/StandAloneTest.java
+++ MavenScrach/src/test/java/seleium/MavenScrach/StandAloneTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -23,18 +25,17 @@ public class StandAloneTest {
 		driver.findElement(By.id("userEmail")).sendKeys("tom@example.com");
 		driver.findElement(By.id("userPassword")).sendKeys("Tom@1234");
 		driver.findElement(By.id("login")).click();
-		
-		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
-		
-		//Optional<String> actualProduct = products.stream().map(product->product.findElement(By.cssSelector("b")).getText()).findFirst();
-		
-		//System.out.println("Product Nmae -: "+ actualProduct.isEmpty());
-		
+		WebDriverWait waite = new WebDriverWait(driver,Duration.ofSeconds(5)); 
+		waite.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));	
 		WebElement actualProduct = products.stream().filter(product->
-		product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
-		
+		product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);		
 		actualProduct.findElement(By.cssSelector(".card-body button:last-of-type")).click(); // agr 2 button h to 2 vala find hoga.
-		
+		// Check if flash message showing or not after add product  into cart
+		waite.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		// check the loader is invisible or not
+		waite.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
 	}
 
 }
