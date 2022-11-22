b7c13a6b55fe9efdda42e36eb592ac139a174842
SanjuDeveloper <bhattsanju41@gmail.com>
2022-11-22 09:41:02 -0800

    print with paginations

--- MavenScrach/src/test/java/tests/Demo.java
+++ MavenScrach/src/test/java/tests/Demo.java
@@ -13,6 +13,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Demo {
 
+	public static boolean isDisplay;
+	public static boolean isEnable;
+	
 	@Test
 	public  void GetAllproducts() {
 		// TODO Auto-generated constructor stub
@@ -22,10 +25,24 @@ public class Demo {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.amazon.in/");
 		driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("Phone",Keys.ENTER);
+		
+		//System.out.println("The 16th Products IS = "+ allProducts.get(15).getText());
+		 isDisplay = driver.findElement(By.cssSelector("a[aria-label='Go to next page, page 2']")).isDisplayed();
+		 isEnable = driver.findElement(By.cssSelector("a[aria-label='Go to next page, page 2']")).isEnabled();
+		if(isDisplay && isEnable ) {
+			PrintProductsName(driver);
+		}
+		
+	}
+	
+	// print all products with pagination
+	public static void PrintProductsName(WebDriver driver) {
 		List<WebElement> allProducts = driver.findElements(By.cssSelector("span[class='a-size-medium a-color-base a-text-normal']"));
 		allProducts.forEach(name->System.out.println(name.getText()));
-		System.out.println("The 16th Products IS = "+ allProducts.get(15).getText());
-		
+		driver.findElement(By.cssSelector("a[aria-label*='next page']")).click();
+		if(isDisplay && isEnable ) {
+			PrintProductsName(driver);
+		}
 	}
 
 }
