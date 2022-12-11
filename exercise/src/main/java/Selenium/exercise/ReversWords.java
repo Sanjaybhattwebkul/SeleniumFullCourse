68baf9a2f710d0f9dfb7e9afa2c04a940126c810
SanjuDeveloper <bhattsanju41@gmail.com>
2022-12-11 04:38:51 -0800

    String Revers program added

--- exercise/src/main/java/Selenium/exercise/ReversWords.java
+++ exercise/src/main/java/Selenium/exercise/ReversWords.java
@@ -3,14 +3,9 @@ package Selenium.exercise;
 public class ReversWords {
 
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		String name = "SANJAY BHATT WEBKUL";
-		
-		System.out.println(name); 
-		
-		String[] nameArray = name.split(" ");
-		
+		String[] nameArray = name.split(" ");		
 		for (int i=nameArray.length-1; i>=0; i--) {
 			System.out.print(nameArray[i]+" ");
 		
