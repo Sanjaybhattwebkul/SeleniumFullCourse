438e7b26fe8f26bd1a1c9fc888aa96586a4f48b3
SanjuDeveloper <bhattsanju41@gmail.com>
2022-12-25 08:58:49 -0800

    Print Only alphabet in string

--- exercise/src/test/java/Selenium/exercise/PrintAlphabetOnly.java
+++ exercise/src/test/java/Selenium/exercise/PrintAlphabetOnly.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 
 public class PrintAlphabetOnly {
 
+	/*
+	 * Print only Alphabets from string
+	 */
 	public static void main(String[] args) {
 		Scanner sc  = new Scanner(System.in);
 		System.out.println("Enter Any String");
 		String str = sc.nextLine().replaceAll("[0-9]", "");
 		System.out.println(str);
 	}
-
+	
+	/*
+	 * Print only numbers in string 
+	 */
+	public void printNumbers(String str) {
+		String sumbers = str.replaceAll("[^0-9]", "");
+		System.out.println(sumbers);
+	}
 }
