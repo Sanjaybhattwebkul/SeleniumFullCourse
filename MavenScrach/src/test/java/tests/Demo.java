e7364a7721a49c2d8c136c8cbf712a0c9d94c053
SanjuDeveloper <bhattsanju41@gmail.com>
2023-02-09 10:09:57 -0800

    Cucumber start

--- MavenScrach/src/test/java/tests/Demo.java
+++ MavenScrach/src/test/java/tests/Demo.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import java.util.*;
+import java.util.Map.Entry;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -23,15 +24,45 @@ public class Demo {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.amazon.in/");
-		driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("Phone",Keys.ENTER);
-		
-		//System.out.println("The 16th Products IS = "+ allProducts.get(15).getText());
-		 isDisplay = driver.findElement(By.cssSelector("a[aria-label='Go to next page, page 2']")).isDisplayed();
-		 isEnable = driver.findElement(By.cssSelector("a[aria-label='Go to next page, page 2']")).isEnabled();
-		if(isDisplay && isEnable ) {
-			PrintProductsName(driver);
+		String URL =  "https://www.flipkart.com";
+		driver.get(URL);
+		String input = "core i5";
+		driver.findElement(By.name("q")).sendKeys(input,Keys.ENTER);
+		List<WebElement> productCard = driver.findElements(By.className("//div[@class='_4ddWXP']"));
+		double  minPrice = 0;
+		HashMap<String,Double> map = new HashMap<String,Double>();
+		for(int i=0; i <productCard.size(); i++) {			
+			String outPutName = productCard.get(i).findElement(By.xpath("//a[2]")).getText();		    
+		 	if(outPutName.contains(input)) {
+		        String  previous = productCard.get(i).findElement(By.xpath("//a[2]/div[2]")).getText().replaceAll("[0-9]", "");
+		        String  next = productCard.get(i+1).findElement(By.xpath("//a[2]/div[2]")).getText().replaceAll("[0-9]", "");		
+		       double previosPrice= Double.parseDouble(previous);		       
+		       double NextPrice= Double.parseDouble(next);		       
+		       if(previosPrice > NextPrice) {
+		    	   minPrice = NextPrice;
+		       }
+		       map.put(outPutName, previosPrice);		      
+		 	}		 	
 		}
+		
+	 	for(Entry<String, Double> entry:map.entrySet()) {
+	 		if(entry.getValue()==minPrice) {
+	 			System.out.println(entry.getKey()+" "+entry.getValue());
+	 		}
+	 	}
+		
+		
+		
+		
+//		driver.get("https://www.amazon.in/");
+//		driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("Phone",Keys.ENTER);
+//		
+//		//System.out.println("The 16th Products IS = "+ allProducts.get(15).getText());
+//		 isDisplay = driver.findElement(By.cssSelector("a[aria-label='Go to next page, page 2']")).isDisplayed();
+//		 isEnable = driver.findElement(By.cssSelector("a[aria-label='Go to next page, page 2']")).isEnabled();
+//		if(isDisplay && isEnable ) {
+//			PrintProductsName(driver);
+//		}
 		
 	}
 	
