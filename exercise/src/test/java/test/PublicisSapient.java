bbedce18afe0857420d65368d83ab0d0a4301f30
SanjuDeveloper <bhattsanju41@gmail.com>
2023-02-16 09:29:07 -0800

    publicis sapient interview question

--- exercise/src/test/java/test/PublicisSapient.java
+++ exercise/src/test/java/test/PublicisSapient.java
@@ -1,19 +1,10 @@
 package test;
-/*
-public class RevString {
 
-	public static void main(String[] args) {
-		
-
-	}
-
-}
-*/
 import java.io.*;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map.Entry;
+
 public class PublicisSapient {
    public static void main(String[] args) throws IOException {
 	  
